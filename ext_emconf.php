0118ff72bb8b3a1c4a14df41da4c13e03bb0f650
Sven Wappler <wappler@wappler.systems>
2026-04-14 11:52:43 +0200

    Update dependencies for TYPO3 14

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,10 +12,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '13.0.0',
+    'version' => '14.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.4.0-13.4.99',
+            'typo3' => '14.0.0-14.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
